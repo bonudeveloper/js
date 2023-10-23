34d9326c98eefa9546f50355879471f7b02bc180
Kumush Xusanova <127647054+bonudeveloper@users.noreply.github.com>
2023-10-23 21:21:43 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let fName = prompt('ismingiz?');
+let lName = prompt('familiyangiz?');
+let dateBirth = prompt('tugilgan oy yil kun');
+let age = prompt('yoshingiz?');
+let gender = prompt('jinsingiz?');
+let edu = prompt('malumotingiz?');
+let work = prompt('hozirgi holatingiz(kasbingiz)?');
+let interests = prompt('qiziqishlaringiz?');
+let address = prompt('manzilingiz?');
+let dream = prompt('orzularingiz?');
+
+console.log(
+  "ism:" + fName +
+    "\n familiya:" + lName +
+    "\n tugilgan yil oy kun: " + dateBirth +
+    "\n yosh: " + age +
+    "\n jins: " + gender +
+    "\n malumotim: " + edu +
+    "\n hozirgi holatim: " + work +
+    "\n qiziqishlarim: " + interests +
+    "\n manzil: " + address +
+    "\n orzu: " + dream
+);
